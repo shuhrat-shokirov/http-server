60eba9a1ebaf07c75cf95732075581bc8185a12a
Shuhrat.Shokirov <avatar_9999@list.ru>
2020-02-23 09:15:59 +0300

    added Test

--- http/server.go
+++ http/server.go
@@ -6,24 +6,43 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"strings"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", "0.0.0.0:9999")
-	if err != nil {
-		return
+	host := "0.0.0.0"
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "9999"
 	}
+	err := start(fmt.Sprintf("%s:%s", host, port))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func start(addr string) (err error) {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return fmt.Errorf("can't listen %s: %w", addr, err)
+	}
+	defer listener.Close()
 	for {
 		conn, err := listener.Accept()
+		log.Print("accept connection")
 		if err != nil {
-			return
+			log.Printf("can't accept: %v", err)
+			continue
 		}
+		log.Print("handle connection")
 		handleConn(conn)
 	}
 }
 
+
 func handleConn(conn net.Conn) {
+	defer conn.Close()
 	reader := bufio.NewReader(conn)
 	requestLine, err := reader.ReadString('\n')
 	if err != nil {
@@ -40,7 +59,6 @@ func handleConn(conn net.Conn) {
 		writer := bufio.NewWriter(conn)
 		writer.WriteString("HTTP/1.1 200 Ok\r\n")
 		writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(html)))
-		// мы идём по простому и закрываем (установление соединения - keep-alive (не закрывать соединения)
 		writer.WriteString("Content-Type: text/html\r\n")
 		writer.WriteString("Connection: close\r\n")
 		writer.WriteString("\r\n")
---
+++ http/server_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+"bufio"
+"io/ioutil"
+"net"
+"strings"
+"testing"
+"time"
+)
+
+func Test_server(t *testing.T) {
+	// naive approach
+	go func() {
+		err := start("localhost:9999")
+		if err != nil {
+			t.Fatalf("can't start server: %v", err)
+		}
+	}()
+	// FIXME: hardcoded approach
+	time.Sleep(1_000_000_000)
+	conn, err := net.Dial("tcp", "localhost:9999")
+	if err != nil {
+		t.Fatalf("can't connect to server: %v", err)
+	}
+	defer conn.Close()
+	writer := bufio.NewWriter(conn)
+	writer.WriteString("GET / HTTP/1.1\r\n")
+	writer.WriteString("Host: localhost\r\n")
+	writer.WriteString("\r\n")
+	writer.Flush()
+	bytes, err := ioutil.ReadAll(conn)
+	if err != nil {
+		t.Fatalf("can't read response from server: %v", err)
+	}
+	response := string(bytes)
+	if !strings.Contains(response, "HTTP/1.1 200 Ok") {
+		t.Fatalf("non-success response: %s", response)
+	}
+}
